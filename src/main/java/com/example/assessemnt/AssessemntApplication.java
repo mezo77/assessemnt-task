458e8fb36afbc5239477719e25d360460841c0ee
Muataz Mahmoud <developer.muataz.mahmoud@gmail.com>
2025-11-26 22:57:14 +0100

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/example/assessemnt/AssessemntApplication.java
@@ -0,0 +1,13 @@
+package com.example.assessemnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessemntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessemntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/assessemnt/AssessemntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assessemnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessemntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
